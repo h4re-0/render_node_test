b386ad627db194c3f04e99b874fd1e00eb800eee
h4re-0 <h4re_0_o@proton.me>
2023-10-16 11:28:43 +0900

    postgres connect test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -8,6 +9,31 @@ const server = app.listen(port, () => console.log(`Example app listening on port
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
+
+
+app.get('/staff', async (req, res) => {
+  const pgOption = {
+    user: process.env.pguser,
+    host: process.env.pghost,
+    database: process.env.pgdatabase,
+    password: process.env.pgpassword,
+    port: process.env.pgport,  
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+  
+  console.log(pgOption);
+  const pool = new Pool(pgOption);
+  
+  try {
+    const result = await pool.query('SELECT * FROM staff');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 const html = `
 <!DOCTYPE html>
